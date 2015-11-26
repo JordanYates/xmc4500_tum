1a43cde724b3c663c6f240b4e70028ababd838a7
Jordan Yates <jordan.yates@uqconnect.edu.au>
2015-11-27 02:08:16 +1000

    Modified main.c to echo data

--- printf/main.c
+++ printf/main.c
@@ -20,8 +20,12 @@ void tiny_putc(void* p, char c)
 }
 
 int main(void) {
+	uint16_t bytesToRead;
+	char buffer[32];
+	uint32_t bufferOffset = 0;
+
 	uint64_t count;
-	int i;
+	int i = 0;
 
 	XMC_SCU_CLOCK_EnableUsbPll();
 	XMC_SCU_CLOCK_StartUsbPll(2, 64);
@@ -35,8 +39,23 @@ int main(void) {
 	init_printf(NULL, tiny_putc);
 
 	while(1) {
-		debug_printf("Test String: %d\r\n", i++);
+		// Get number of bytes to read
+		bytesToRead = CDC_Device_BytesReceived(&VirtualSerial_CDC_Interface);
+		// Load bytes into buffer
+		bufferOffset = 0;
+		while(bytesToRead-- > 0) {
+			buffer[bufferOffset++] = CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+		}
+		// Ensure Null Terminated
+		buffer[bufferOffset] = '\0';
 
+		// Send out an acknowledgment
+		if (bufferOffset == 0) {
+			debug_printf("Test String: %d\r\n", i++);
+		} else {
+			debug_printf("Received %d bytes: %s\r\n", bufferOffset, buffer);
+		}
+		// Wait for approximately 1 second
 		count = 0x8FFFF0;
 		while(--count) ;
 	}
